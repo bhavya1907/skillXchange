fffbf4a1882661f43c775af898a9512026906e4f
bhavya1907 <bhavyaverma.sv@gmail.com>
2026-02-02 23:28:05 +0530

    Update requestsController.js

--- skillexchange/server/apis/requests/requestsController.js
+++ skillexchange/server/apis/requests/requestsController.js
@@ -1,4 +1,19 @@
 const requestsModel = require("./requestsModel")
 const add = (req,res) =>{
     var errMsgs = []
+    if(!req.body.title){
+        errMsgs.push("title is required!!")
+    }
+    if(!req.body.sessionId){
+        errMsgs.push("sessionId is required!!")
+    }
+    if(!req.body.userId){
+        errMsgs.push("userId is required!!")
+    }
+   if (errMsgs.length > 0) {
+        res.send({
+            status: 422,
+            success: false,
+            message: errMsgs
+        });
 }
